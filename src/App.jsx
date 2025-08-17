7d5df3f4bb80d443b4e003a6d1857d7f57b245fd
shishirps7 <sizaleen77@gmail.com>
2025-08-17 23:08:57 +0530

    push1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './nav'
+import Hero from './Hero'
+function App() {
+  
+  return (
+    <>
+      <Nav/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './Hero.css';
+
+// import required modules
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+
+const Hero = () => {
+  return (
+    <div className='Hero'>
+        <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="main" style={{
+      backgroundImage: "url(https://demo-milano.myshopify.com/cdn/shop/files/fs_new_s1.webp?v=1748226426&width=1500)",
+      backgroundSize: "cover",
+      backgroundPosition: "center"
+    }}>
+            <div>
+                <h5>MODERN EVERYDAY LOOKS</h5>
+                <h1>Soft Comfort Bold Looks</h1>
+                <button>Shop Collection</button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="main wide" style={{
+      backgroundImage: "url(https://demo-milano.myshopify.com/cdn/shop/files/fs_new_s2.webp?v=1748226426&width=1500)",
+      backgroundSize: "cover",
+      backgroundPosition: "center"
+    }}>
+            <div>
+                <h5>ELEVATE YOUR STYLE</h5>
+                <h1>Women's Fashion Collection</h1>
+                <button>Shop Collection</button>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
+
+export default Hero
